00705735925b9022648bf7b8de8bee57d487b0e1
Marc Vidal <mvidal.dp@gmail.com>
2019-11-20 11:38:40 +0100

    Add pow and mot data streams, store into CSV file both streams

--- csharp/EEGLogger/Program.cs
+++ csharp/EEGLogger/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using CortexAccess;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
+using System.Timers;
 
 namespace EEGLogger
 {
@@ -13,6 +14,9 @@ namespace EEGLogger
     {
         const string OutFilePath = @"EEGLogger.csv";
         private static FileStream OutFileStream;
+        private static Dictionary<string, ArrayList> wholeData = new Dictionary<string, ArrayList>();
+        private static System.Timers.Timer aTimer;
+        
 
         static void Main(string[] args)
         {
@@ -25,12 +29,14 @@ namespace EEGLogger
                 File.Delete(OutFilePath);
             }
             OutFileStream = new FileStream(OutFilePath, FileMode.Append, FileAccess.Write);
-
-
+            
             DataStreamExample dse = new DataStreamExample();
-            dse.AddStreams("eeg");
+            dse.AddStreams("mot");
+            dse.AddStreams("pow");
             dse.OnSubscribed += SubscribedOK;
-            dse.OnEEGDataReceived += OnEEGDataReceived;
+            //dse.OnEEGDataReceived += OnEEGDataReceived;
+            dse.OnBandPowerDataReceived += OnBandPowerDataReceived;
+            dse.OnMotionDataReceived += OnMotionDataReceived;
             dse.Start();
 
             Console.WriteLine("Press Esc to exit");
@@ -49,27 +55,52 @@ namespace EEGLogger
 
         private static void SubscribedOK(object sender, Dictionary<string, JArray> e)
         {
+            SetTimer();
+            ArrayList header = new ArrayList();
+            header.Insert(0, "Timestamp");
             foreach (string key in e.Keys)
             {
-                if (key == "eeg")
+                switch (key)
                 {
-                    // print header
-                    ArrayList header = e[key].ToObject<ArrayList>();
-                    //add timeStamp to header
-                    header.Insert(0, "Timestamp");
-                    WriteDataToFile(header);
+                    case "eeg":
+                        // print header
+                        header.AddRange(e[key].ToObject<ArrayList>());
+                        break;
+                    case "mot":
+                        // print header
+                        header.AddRange(e[key].ToObject<ArrayList>());
+                        break;
+                    case "pow":
+                        // print header
+                        header.AddRange(e[key].ToObject<ArrayList>());
+                        break;
                 }
             }
+            WriteHeaderToFile(header);
+        }
+        
+        private static void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(4000);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
         }
 
-        // Write Header and Data to File
-        private static void WriteDataToFile(ArrayList data)
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            WriteDataToFile();
+        }
+
+        // Write Header to File
+        private static void WriteHeaderToFile(ArrayList data)
         {
             int i = 0;
             for (; i < data.Count - 1; i++)
             {
                 byte[] val = Encoding.UTF8.GetBytes(data[i].ToString() + ", ");
-
                 if (OutFileStream != null)
                     OutFileStream.Write(val, 0, val.Length);
                 else
@@ -77,13 +108,55 @@ namespace EEGLogger
             }
             // Last element
             byte[] lastVal = Encoding.UTF8.GetBytes(data[i].ToString() + "\n");
-            if (OutFileStream != null)
-                OutFileStream.Write(lastVal, 0, lastVal.Length);
+            OutFileStream?.Write(lastVal, 0, lastVal.Length);
+        }
+        
+        // Write Header to File
+        private static void WriteDataToFile()
+        {
+            foreach (var element in wholeData)
+            {
+                byte[] time = Encoding.UTF8.GetBytes(element.Key + ",");
+                if (OutFileStream != null)
+                    OutFileStream.Write(time, 0, time.Length);
+                else
+                    break;
+                int i = 0;
+                for (; i < element.Value.Count; i++)
+                {
+                    byte[] val = Encoding.UTF8.GetBytes(element.Value[i] + ",");
+                    OutFileStream?.Write(val, 0, val.Length);
+                }
+                // Last element
+                byte[] breakLine = Encoding.UTF8.GetBytes("\n");
+                OutFileStream?.Write(breakLine, 0, breakLine.Length);
+            }
         }
 
         private static void OnEEGDataReceived(object sender, ArrayList eegData)
         {
-            WriteDataToFile(eegData);
+            StoreData(eegData);
+        }
+        
+        private static void OnBandPowerDataReceived(object sender, ArrayList powerData)
+        {
+            StoreData(powerData);
+        }
+        
+        private static void OnMotionDataReceived(object sender, ArrayList motionData)
+        {
+            StoreData(motionData);
+        }
+
+        private static void StoreData(ArrayList dataToAdd)
+        {
+            string time = dataToAdd[0].ToString();
+            dataToAdd.RemoveAt(0);
+            if (wholeData.ContainsKey(time))
+                wholeData[time].AddRange(dataToAdd);
+            else
+                wholeData.Add(time, dataToAdd);
+
         }
 
     }
