4bf4d2f5ceb42375b321bb38e44322fc5b05cb3b
Marc Vidal <mvidal.dp@gmail.com>
2019-11-20 11:13:50 +0100

    Fix wrong key parameter license_id->license

--- python/lib/cortex.py
+++ python/lib/cortex.py
@@ -199,7 +199,7 @@ class Cortex(object):
         params = {'clientId': self.client_id,
                   'clientSecret': self.client_secret}
         if license_id:
-            params['license_id'] = license_id
+            params['license'] = license_id # commit this change instead of license_id as params key it is license. Add into the issue as a fix but not resolved
         if debit:
             params['debit'] = debit
 
